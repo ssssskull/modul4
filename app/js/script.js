a6b6a200e860c70c27219c13db744b21c3c839a8
Daniel Papp <danipoker1@gmail.com>
2021-02-11 16:52:24 +0100

    Toggle is made with js now

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,13 @@
-const btnHamburger = document.querySelector("#hamburger");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector('.header');
+
 btnHamburger.addEventListener('click', function () {
     console.log('click hamburger');
+
+    if (header.classList.contains('open')) { // Close Hamburger Menu
+        header.classList.remove('open');
+    }
+    else { // Open Hamburger Menu
+        header.classList.add('open');
+    }
 });  
