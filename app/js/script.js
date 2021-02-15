1b4b80feb7020e59d2e7b05ee11b4ccb8deffc95
Daniel Papp <danipoker1@gmail.com>
2021-02-15 16:31:09 +0100

    Trying to fix 100vh issue

--- app/js/script.js
+++ app/js/script.js
@@ -44,3 +44,10 @@ LottieInteractivity.create({
 
 let scrollToContent = document.getElementById("#scrollToContent");
 let mainContainer = document.getElementById("#mainContainer");
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
