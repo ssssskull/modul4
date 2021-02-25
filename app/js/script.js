b1836c11e3f424ca2cad3cf79846ff973b2dfc5c
frederikke-m <frederikke.b.mathiesen@gmail.com>
2021-02-25 17:16:46 +0100

    JS – cirkler i progressbar skifter farve, når man trykker på dem

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,3 @@
-
 console.log("hello");
 // Hamburger toggle
 const btnHamburger = document.querySelector(".header__toggle");
@@ -77,32 +76,134 @@ LottieInteractivity.create({
 /*not sure if this works lol setTimeout(function () { animation.play(); }, 20000);*/
 
 // Fem faser js
-let nummer = document.querySelector("#h1femfaser").value;
-console.log(typeof nummer)
-nummer.padStart(2, '0')
 
 
-function incrementNummer() {
-  document.querySelector("#h1femfaser").value = nummer += 2;
-}
-
-let button = document.querySelector("button");
+let button = document.querySelector(".arrowright");
+console.log(button);
 button.addEventListener("click", incrementNummer)
 
 
-// Smooth Scroll Safari
+function incrementNummer() {
+  console.log("helloooooo");
+  //Vi starter med en string -- når vi trykker på knappen skal vi så konvertere værdien til et nummer --> increment --> lav om til string --> tilføj 0 foran
+  let input = document.querySelector("#h1femfaser").innerHTML;
+  input = parseFloat(input);
+  input = input++;
+  console.log(typeof input);
+  input = String(input);
+  console.log(typeof input);
+  //input.value = ++input;
 
-let scrollToContent = document.getElementById("#scrollToContent");
-let mainContainer = document.getElementById("#mainContainer");
+  //console.log(input.padStart(2, '0'));
+}
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+let faser = Array.from(document.querySelectorAll('.femfasercircle'));
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
+const handleClick = (e) => {
+  e.preventDefault();
+  faser.forEach(node => {
+    node.classList.remove('active');
+  });
+  e.currentTarget.classList.add('active');
+
+  console.log(faser[0]);
+  if (faser[0].classList.contains("active")) {
+    faser[0].style.background = "#A5BA74";
+    faser[1].style.background = "#e0e0e0";
+    faser[2].style.background = "#e0e0e0";
+    faser[3].style.background = "#e0e0e0";
+    faser[4].style.background = "#e0e0e0";
+  }
+
+  else if (faser[1].classList.contains("active")) {
+    faser[1].style.background = "#D8A55B";
+    faser[0].style.background = "#e0e0e0";
+    faser[2].style.background = "#e0e0e0";
+    faser[3].style.background = "#e0e0e0";
+    faser[4].style.background = "#e0e0e0";
+  }
+
+  else if (faser[2].classList.contains("active")) {
+    faser[2].style.background = "#AA82AD"
+    faser[0].style.background = "#e0e0e0";
+    faser[1].style.background = "#e0e0e0";
+    faser[3].style.background = "#e0e0e0";
+    faser[4].style.background = "#e0e0e0";
+  }
+
+  else if (faser[3].classList.contains("active")) {
+    faser[3].style.background = "#6696AD"
+    faser[0].style.background = "#e0e0e0";
+    faser[1].style.background = "#e0e0e0";
+    faser[2].style.background = "#e0e0e0";
+    faser[4].style.background = "#e0e0e0";
+  }
+
+  else if (faser[4].classList.contains("active")) {
+    faser[4].style.background = "#D67249"
+    faser[0].style.background = "#e0e0e0";
+    faser[1].style.background = "#e0e0e0";
+    faser[2].style.background = "#e0e0e0";
+    faser[3].style.background = "#e0e0e0";
+  }
+}
+
+  faser.forEach(node => {
+    node.addEventListener('click', handleClick);
+  });
+
+
+
+  //const header = document.querySelector('.header');
+
+  // btnHamburger.addEventListener('click', function () {
+  //   if (header.classList.contains('open')) { // Close Hamburger Menu
+  //     header.classList.remove('open');
+  //   }
+  //   else { // Open Hamburger Menu
+  //     header.classList.add('open');
+  //   }
+  // });
+
+
+  /*input = parseFloat(input);
+  
+  
+  
+  function next_id(input) {
+    var output = parseInt(input, 10) + 1; // parse and increment
+    output += ""; // convert to string
+    while (output.length < 2) output = "0" + output; // prepend leading zeros
+    return output;
+  }
+  
+  const id = "00";
+  for (var increment = 0; increment < 20; increment++) {
+    console.log(id);
+    id = next_id(id);
+  }
+  
+  /*function incrementNummer() {
+    document.querySelector("#h1femfaser").value = nummer += 2;
+  }
+  
+  let button = document.querySelector("button");
+  button.addEventListener("click", incrementNummer)
+  */
+
+  // Smooth Scroll Safari
+
+  let scrollToContent = document.getElementById("#scrollToContent");
+  let mainContainer = document.getElementById("#mainContainer");
+
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
   let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
