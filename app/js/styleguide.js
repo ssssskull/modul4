ad9c6d5061c0344a7931eed0b520977f301b0b23
Victor Sehested <victorsehested@gmail.com>
2021-03-03 11:30:28 +0100

    conesole.log, If Else & loop done

---
+++ app/js/styleguide.js
@@ -0,0 +1,39 @@
+
+// Victor
+// log, If Else & loop 
+
+let vfirstName = "Victor";
+let vsurname = "Sehested";
+
+let vfullName = vfirstName + ' ' + vsurname;
+
+if (vfullName == "Victor Sehested") {
+    console.log(`Your name is ${vfullName}`);
+  } else {
+    console.log(`Your name isn't ${vfullName}`)
+  };
+
+var vshoppingList = ["Mælk", "Æg", "Ris", "Brød", "Bacon"];
+var loop
+
+console.log(loop);
+
+for (loop = 0; loop < 4; loop++) {
+    console.log(`The number is ${loop}`);
+}
+
+console.log(loop);
+
+// Emelie
+
+
+
+
+
+
+// Sara
+
+
+
+
+
