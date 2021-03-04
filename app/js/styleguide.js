ca2a345ea2fddfb2a9d61918201e95bd9dee393a
Daniel Papp <danipoker1@gmail.com>
2021-03-04 14:52:26 +0100

    added styleguide js file

--- app/js/styleguide.js
+++ app/js/styleguide.js
@@ -92,6 +92,29 @@ for (emelie = 0; emelie < kanlide.length; emelie++) {
 
 
 
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+        if(this.checked) {
+            trans()
+            document.documentElement.setAttribute('data-theme', 'dark')
+
+        } else {
+        trans()
+            document.documentElement.setAttribute('data-theme', 'light')
+
+        }
+    })
+
+    let trans = () => {
+        document.documentElement.classList.add('transition');
+        window.setTimeout(() => {
+            document.documentElement.classList.remove('transition');
+
+        }, 1000)
+    }
+
+
 
 
 
